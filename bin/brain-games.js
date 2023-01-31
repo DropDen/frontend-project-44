2708ef00eb3665ca870140c65eb0c9b07dc64a43
Denis Kaplin <stronzii19@gmail.com>
2023-01-31 20:53:18 +0700

    3 tur done

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!')
+console.log("May I have your name?")
+const name = readlineSync.question('Your answer: ');
+console.log("Hello," + name + "!")
